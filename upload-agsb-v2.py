79cdde8eb65bd69c4bfa619c075734ea623872ff
guomeiooi <guomeiooi163@gmail.com>
2025-06-06 11:28:08 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+USER_NAME = "guomeiooi"         # 用户名
+UUID = "01199bb9-c51a-4f28-aa27-b0d5e52d3ab8"                     # UUID，留空则自动生成
+PORT = 43395                   # Vmess端口，留空或0则自动生成
+DOMAIN = "gugeyun001.xxcardinalcommerce.com"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiZDZlMjA0NDAzZjRlZDQ3YTYxM2JmZjRkODAxNTMxYjkiLCJ0IjoiYzAyZjA3YTYtNmUzZS00NmYyLThjZWQtNGIwOTA5MGI2NmFiIiwicyI6Ik5XSTJORFV5T0RjdE5qWTFaQzAwT0dNMkxXSm1ZVFF0T1dObE9HUmlaVFZpWVRaaiJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
